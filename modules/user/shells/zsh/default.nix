d505268f536aebc484d9098b68cb9ea71addf314
connor-boucher <60579609+connor-boucher@users.noreply.github.com>
2024-11-16 01:25:31 +0000

    extended config for zsh

--- modules/user/shells/zsh/default.nix
+++ modules/user/shells/zsh/default.nix
@@ -6,6 +6,27 @@
   config = {
     programs.zsh = lib.mkIf config.zsh.enable {
       enable = true;
+
+      # Set custom prompt
+      initExtra = ''
+      autoload -U colors && colors
+      PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
+      '';
+
+      # Enable vi mode
+      defaultKeymap = "viins";
+
+      # Configuration settings
+      autocd = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      autosuggestion.strategy = [ "history" "completion" ];
+      syntaxHighlighting.enable = true;
+
+      # Fish-style abbreviations
+      zsh-abbr.enable = true;
+      zsh-abbr.abbreviations = {
+      };
     };
   };
 }
